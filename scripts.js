b9c38a8d297b72cab3c51de98d099b2ccde8e2c7
jtphillips215 <jtphillips215@gmail.com>
2023-05-04 18:21:01 -0500

    added mathmatic operations in js file

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
 "use strict";
 
-// contains scripts needed to run calculator app
+// mathmatic operations
+function addition(operator, firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function subtraction(operator, firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+function multiplication(operator, firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function division(operator, firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+function percentage(operator, firstNumber, secondNumber) {
+  return (firstNumber / 100) * secondNumber;
+}
