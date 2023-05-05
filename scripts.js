679ca60920389605d4cd70236a5ef6b2e6b14ac3
jtphillips215 <jtphillips215@gmail.com>
2023-05-04 19:54:10 -0500

    added switch statement for calculate function, cleaned up other function parameters

--- scripts.js
+++ scripts.js
@@ -1,22 +1,51 @@
 "use strict";
 
 // mathmatic operations
-function addition(operator, firstNumber, secondNumber) {
+function addition(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 }
 
-function subtraction(operator, firstNumber, secondNumber) {
+function subtraction(firstNumber, secondNumber) {
   return firstNumber - secondNumber;
 }
 
-function multiplication(operator, firstNumber, secondNumber) {
+function multiplication(firstNumber, secondNumber) {
   return firstNumber * secondNumber;
 }
 
-function division(operator, firstNumber, secondNumber) {
+function division(firstNumber, secondNumber) {
   return firstNumber / secondNumber;
 }
 
-function percentage(operator, firstNumber, secondNumber) {
+function percentage(firstNumber, secondNumber) {
   return (firstNumber / 100) * secondNumber;
+}
+
+// calculate called by on click listener for equals sign
+function calculate(operator, firstNumber, secondNumber) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = addition(firstNumber, secondNumber);
+      break;
+    case "-":
+      result = subtraction(firstNumber, secondNumber);
+      break;
+    case "*":
+      result = multiplication(firstNumber, secondNumber);
+      break;
+    case "/":
+      if (secondNumber != 0) {
+        result = division(firstNumber, secondNumber);
+        break;
+      } else {
+        result = "Div by Zero Error";
+      }
+    case "%":
+      result = percentage(firstNumber, secondNumber);
+      break;
+    default:
+      break;
+  }
+  return result;
 }
